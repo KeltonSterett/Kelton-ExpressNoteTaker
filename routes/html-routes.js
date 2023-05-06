dce29765a269648c8b273d7f1a8e61cc870de3bd
KeltonSterett <keltonsterett24@gmail.com>
2023-05-06 01:48:44 -0600

    added public folder to house other folders, and added routes for notes and index html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,3 +3,10 @@ const router = require("express").Router();
 const path = require("path");
 
 // route that sends 'index.html' as a response to the client (user)
+router.get("/", (req, res) => {  
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+// route that sends 'notes.html' as a response to the client (user)
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
