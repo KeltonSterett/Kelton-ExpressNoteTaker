cf8208229f71d8b3f1efbdc2ec7e60f2a4128180
KeltonSterett <keltonsterett24@gmail.com>
2023-05-06 01:08:38 -0600

    installed express and added gitignore file

---
+++ routes/html-routes.js
@@ -0,0 +1,5 @@
+// require express and path modules
+const router = require("express").Router();
+const path = require("path");
+
+// route that sends 'index.html' as a response to the client (user)
