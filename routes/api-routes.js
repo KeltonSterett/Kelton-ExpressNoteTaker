8b3301828536cce8e2c9da69662652a74025712b
KeltonSterett <keltonsterett24@gmail.com>
2023-05-06 02:22:44 -0600

    added get and post request in api.js exported router

---
+++ routes/api-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
+// define the get request for the notes page
+router.get("api/notes", async (req, res) => {
+    const dbJson = await JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json (dbJson);
+});
+
+// define the post request for the notes page
+router.post("api/notes", (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+ dbJson.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify (dbJson));
+    res.json(dbJson);
+}); 
+
+module.exports = router;
